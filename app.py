2e67e177f678bc9cc605976e30e9241e1ba7d93e
unknown <louis.chenain@ieseg.fr>
2026-03-16 09:33:28 +0100

    dotenv

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify, request
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()
 
-
-def get_connection():
-    return mysql.connector.connect(
-        host="mysql-serhal.alwaysdata.net",
-        user="serhal",
-        password="968-AJK-0101/",
-        database="serhal_projct"
-    )
+def get_connection(): 
+    conn = mysql.connector.connect( 
+        host=os.getenv("DB_HOST"), 
+        user=os.getenv("DB_USER"), 
+        password=os.getenv("DB_PASSWORD"), 
+        database=os.getenv("DB_NAME") 
+    ) 
+    return conn
 
 
 @app.route("/ping", methods=["GET"])
